040df6946f03ddbd1f6463262cfc5737093fed66
S. Dean Rider Jr <94093691+DeanRider@users.noreply.github.com>
2022-08-18 21:36:29 -0400

    Add files via upload

---
+++ Deduplicator.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#######################################################################
+# 
+# This script will scan a folder to create a list of input fasta files 
+# It will then use each file in the list it created
+# and create a two-column format for each sequence
+# paste puts two lines into a single line with space between them
+# awk sets up a loop through lines and prints if the expression is true
+# ...if the next line is not the same in column 2, it fails to print the line
+# awk results are piped to awk again and it will print column 1, newline, column 2 for fasta format
+# then it will output the print to a new deduplicated file
+# use: bash Deduplicator.sh
+# written August 18, 2022 by S. Dean Rider Jr. based on the following:
+# based on 
+# https://stackoverflow.com/questions/8442459/put-every-alternate-row-in-a-column-using-some-unix-commands
+# https://www.baeldung.com/linux/uniq-by-column
+#######################################################################
+
+# scan directory structure for sam results and store as a list
+find . -name "*.fa" -type file -exec echo '{}' >> DeduplicatorList.txt \;
+
+#loop through list in the file and deduplicata sequences
+while read fastafile; do
+
+echo $fastafile
+
+paste -d " " - - < $fastafile | awk 'NR==1 || !a[$2]++' | awk '{print $1"\n"$2}' > $fastafile.dedup.fa || true
+
+
+echo -e "\033[1;34m -. .-.   .-. .-.   .-. .-.   .-. .-.   .-. .-.   .-. .-.   .\033[0m";
+echo -e "\033[1;30m ||\|||\ /|||\|||\ /|||\|||\ /|||\|||\ /|||\|||\ /|||\|||\ /|\033[0m";
+echo -e "\033[1;30m |/ \|||\|||/ \|||\|||/ \|||\|||/ \|||\|||/ \|||\|||/ \|||\||\033[0m";
+echo -e "\033[1;31m ~   \`-~ \`-\`   \`-~ \`-\`   \`-~ \`-~   \`-~ \`-\`   \`-~ \`-\`   \`-~ \`-\033[0m";
+
+done < "DeduplicatorList.txt"
+
+rm DeduplicatorList.txt
+
+echo
+echo
+echo
+echo -e "\033[1;31m ▄▄▄▄▄▄▄▄     ▄▄▄▄▄▄▄▄▄▄▄  ▄▄        ▄  ▄▄▄▄▄▄▄▄▄▄▄ \033[0m";
+echo -e "\033[1;31m▐░░░░░░░░▌   ▐░░░░░░░░░░░▌▐░░▌      ▐░▌▐░░░░░░░░░░░▌\033[0m";
+echo -e "\033[1;31m▐░█▀▀▀▀▀█░▌  ▐░█▀▀▀▀▀▀▀█░▌▐░▌░▌     ▐░▌▐░█▀▀▀▀▀▀▀▀▀ \033[0m";
+echo -e "\033[1;31m▐░▌      ▐░▌ ▐░▌       ▐░▌▐░▌▐░▌    ▐░▌▐░▌          \033[0m";
+echo -e "\033[1;31m▐░▌       ▐░▌▐░▌       ▐░▌▐░▌ ▐░▌   ▐░▌▐░█▄▄▄▄▄▄▄▄▄ \033[0m";
+echo -e "\033[1;31m▐░▌       ▐░▌▐░▌       ▐░▌▐░▌  ▐░▌  ▐░▌▐░░░░░░░░░░░▌\033[0m";
+echo -e "\033[1;31m▐░▌       ▐░▌▐░▌       ▐░▌▐░▌   ▐░▌ ▐░▌▐░█▀▀▀▀▀▀▀▀▀ \033[0m";
+echo -e "\033[1;31m▐░▌      ▐░▌ ▐░▌       ▐░▌▐░▌    ▐░▌▐░▌▐░▌          \033[0m";
+echo -e "\033[1;31m▐░█▄▄▄▄▄█░▌  ▐░█▄▄▄▄▄▄▄█░▌▐░▌     ▐░▐░▌▐░█▄▄▄▄▄▄▄▄▄ \033[0m";
+echo -e "\033[1;31m▐░░░░░░░░▌   ▐░░░░░░░░░░░▌▐░▌      ▐░░▌▐░░░░░░░░░░░▌\033[0m";
+echo -e "\033[1;31m ▀▀▀▀▀▀▀▀     ▀▀▀▀▀▀▀▀▀▀▀  ▀        ▀▀  ▀▀▀▀▀▀▀▀▀▀▀ \033[0m";
+echo -e "\033[1;31m                                                    \033[0m";
+
+
+
+exit 0
+
+
+
